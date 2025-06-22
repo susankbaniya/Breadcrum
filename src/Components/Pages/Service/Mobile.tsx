ea1f1afaec3e36e4c524421eedbd8e09fd5916d2
Susank Baniya <susankbaniya12345@gmail.com>
2025-06-22 08:35:11 +0545

    samdhi

--- src/Components/Pages/Service/Mobile.tsx
+++ src/Components/Pages/Service/Mobile.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import "./Camera.css"
+import React from "react";
+import "./Camera.css";
 const Mobile = () => {
   return (
-      <div className="home">
-       <div className="container"><h1>Mobile </h1>
-        
-       <p className="paragraphcamera">A mobileis a device used to capture images or videos. Cameras work by letting light through a lens onto a sensor (or film), which records the light to form a picture.</p>
-       </div></div>
-  )
-}
+    <div className="home">
+      <div className="container">
+        <h1>Mobile </h1>
+        kina
+        <p className="paragraphcamera">
+          A mobileis a device used to capture images or videos. Cameras work by
+          letting light through a lens onto a sensor (or film), which records
+          the light to form a picture.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Mobile
+export default Mobile;
--- src/Components/Pages/Service/Service.tsx
+++ src/Components/Pages/Service/Service.tsx
@@ -1,16 +1,17 @@
-import { Link } from 'react-router-dom';
-import Camera from './Camera';
-import Mobile from './Mobile';
+import { Link } from "react-router-dom";
+import Camera from "./Camera";
+import Mobile from "./Mobile";
 function Service() {
-
   return (
-    <div className="home">
-   <div className="container">
-      <h2>Services</h2>
-    <Link to="/products/services/camera">Camera details</Link>
-    <div>
-  <Link to="/products/services/mobile">Mobile details</Link></div>
-   </div> </div>
+    <div className="homaaae">
+      <div className="container">
+        <h2>Services</h2>
+        <Link to="/products/services/camera">Camera details</Link>
+        <div>
+          <Link to="/products/services/mobile">Mobile details</Link>
+        </div>
+      </div>{" "}
+    </div>
   );
 }
 
